59b666b43322f351315a9496f785d927bb843c59
Arhen Santiago <lincoldarhen9900@gmail.com>
2024-10-14 16:51:19 +0800

    created page and insert

--- cutcuss.php
+++ cutcuss.php
@@ -6,3 +6,102 @@ Version: 1.0
 Author: Ey Rhen
 License: GPL2
 */
+function cutcuss_activation()
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . "cutcuss_words";
+    $charset_collate = $wpdb->get_charset_collate();
+
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    word tinytext NOT NULL,
+    PRIMARY KEY  (id)
+) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
+register_activation_hook(__FILE__, 'cutcuss_activation');
+function cutcuss_deactivation() {}
+register_deactivation_hook(__FILE__, 'cutcuss_deactivation');
+
+function cutcuss_menu()
+{
+    add_menu_page(
+        'CutCuss Page',
+        'Cutcuss',
+        'manage_options',
+        'cutcuss',
+        'cutcuss_page_html',
+        'dashicons-admin-generic',
+        20
+    );
+}
+add_action('admin_menu', 'cutcuss_menu');
+
+
+
+
+function cutcuss_page_html()
+{
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+
+    if (isset($_GET['status']) && $_GET['status'] == 'success') {
+        echo '<div class="updated notice"><p>Subission Successdul</p></div>';
+    } elseif (isset($_GET['status']) && $_GET['status'] == 'error') {
+        echo '<div class="error notice"><p>Submission Failed</p></div>';
+    }
+
+?>
+    <div class="wrap">
+        <h1>Cutcuss Plugin</h1>
+        <p>Censor the unwanted words in your posts.</p>
+        <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>">
+
+            <input type="hidden" name="action" value="cutcuss_handle_form">
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">Enter a word</th>
+                    <td><input type="text" name="word" /></td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+
+<?php
+}
+
+function cutcuss_handle_form()
+{
+    if (isset($_POST['word']) && !empty($_POST['word'])) {
+
+        $word = sanitize_text_field($_POST['word']);
+        insert_word($word);
+        wp_redirect(admin_url('admin.php?page=cutcuss&status=success'));
+        exit;
+    } else {
+        wp_redirect(admin_url('admin.php?page=cutcuss&status=error'));
+        exit;
+    }
+}
+add_action('admin_post_cutcuss_handle_form', 'cutcuss_handle_form');
+
+function insert_word($word)
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . "cutcuss_words";
+
+    $wpdb->insert(
+        $table_name,
+        array(
+            'word' => $word
+        ),
+        array(
+            '%s', // Data type for 'name' (string)
+        )
+    );
+}
